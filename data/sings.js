f1607910866ea8b37a1687aa0fd8061765e945b0
Edwin Ortiz <edarcode@gmail.com>
2022-07-17 12:53:01 -0500

    delete imgs

--- data/sings.js
+++ data/sings.js
@@ -4,7 +4,8 @@ export const sings = [
     artist: "AC/DC",
     audio:
       "https://res.cloudinary.com/dvmsllkkg/video/upload/v1658077500/apps/black_in_black_a7fjrx.mp3",
-    image: "imgs/black_in_black.jpg",
+    image:
+      "https://res.cloudinary.com/dvmsllkkg/image/upload/v1658080215/apps/black_in_black_ymonwm.webp",
   },
 
   {
@@ -12,35 +13,40 @@ export const sings = [
     artist: "Justin Bieber",
     audio:
       "https://res.cloudinary.com/dvmsllkkg/video/upload/v1658077496/apps/stay_ztjrhu.mp3",
-    image: "imgs/stay.png",
+    image:
+      "https://res.cloudinary.com/dvmsllkkg/image/upload/v1658080216/apps/stay_wudzvz.webp",
   },
   {
     title: "Tom's Diner (Cover)",
     artist: "AnnenMayKantereit / Giant Rooks",
     audio:
       "https://res.cloudinary.com/dvmsllkkg/video/upload/v1658077502/apps/annenMayKantereit_giant_rooks_vd8iox.mp3",
-    image: "imgs/annenMayKantereit_giant.jpg",
+    image:
+      "https://res.cloudinary.com/dvmsllkkg/image/upload/v1658080215/apps/annenMayKantereit_giant_pnva1y.webp",
   },
   {
     title: "No quiero Llorar (Cover)",
     artist: "Maikel Delacalle",
     audio:
       "https://res.cloudinary.com/dvmsllkkg/video/upload/v1658077495/apps/no_quiero_llorar_u9esax.mp3",
-    image: "imgs/maikel_delacalle.jpg",
+    image:
+      "https://res.cloudinary.com/dvmsllkkg/image/upload/v1658080215/apps/maikel_delacalle_xtok9j.webp",
   },
   {
     title: "Love Nwantiti (Lyrics)",
     artist: "Ckay",
     audio:
       "https://res.cloudinary.com/dvmsllkkg/video/upload/v1658077488/apps/love_nwantiti_cfagbu.mp3",
-    image: "imgs/ckay.jfif",
+    image:
+      "https://res.cloudinary.com/dvmsllkkg/image/upload/v1658080215/apps/ckay_il0o3h.webp",
   },
   {
     title: "El Tiempo Pasa",
     artist: "Farruko",
     audio:
       "https://res.cloudinary.com/dvmsllkkg/video/upload/v1658077502/apps/el_tiempo_pasa_eh8shc.mp3",
-    image: "imgs/farruko.jpg",
+    image:
+      "https://res.cloudinary.com/dvmsllkkg/image/upload/v1658080215/apps/farruko_gku9hx.webp",
   },
   {
     title: "Monkey Island (Remix)",
@@ -54,20 +60,23 @@ export const sings = [
     artist: "Extreme",
     audio:
       "https://res.cloudinary.com/dvmsllkkg/video/upload/v1658077496/apps/more_than_words_w3ldoe.mp3",
-    image: "imgs/more_than_words.png",
+    image:
+      "https://res.cloudinary.com/dvmsllkkg/image/upload/v1658080216/apps/more_than_words_hmlppy.webp",
   },
   {
     title: "More Than You Know",
     artist: "❓",
     audio:
       "https://res.cloudinary.com/dvmsllkkg/video/upload/v1658077720/apps/more_than_you_know_uupscw.mp3",
-    image: "imgs/scummbar.jpg",
+    image:
+      "https://res.cloudinary.com/dvmsllkkg/image/upload/v1658080216/apps/scummbar_i44fmg.webp",
   },
   {
     title: "Demons",
     artist: "Imagine Dragons",
     audio:
       "https://res.cloudinary.com/dvmsllkkg/video/upload/v1658076971/apps/demons_qab3wi.mp3",
-    image: "imgs/lechuck.jpg",
+    image:
+      "https://res.cloudinary.com/dvmsllkkg/image/upload/v1658080215/apps/lechuck_g0tgda.webp",
   },
 ];
