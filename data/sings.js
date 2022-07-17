656246350bdc28b02b646938993e3f211bba5023
Edwin Ortiz <edarcode@gmail.com>
2022-07-17 12:10:48 -0500

    delete(/music)

--- data/sings.js
+++ data/sings.js
@@ -2,38 +2,44 @@ export const sings = [
   {
     title: "Black in Black",
     artist: "AC/DC",
-    audio: "/music/black_in_black.mp3",
+    audio:
+      "https://res.cloudinary.com/dvmsllkkg/video/upload/v1658077500/apps/black_in_black_a7fjrx.mp3",
     image: "imgs/black_in_black.jpg",
   },
 
   {
     title: "Stay",
     artist: "Justin Bieber",
-    audio: "/music/stay.mp3",
+    audio:
+      "https://res.cloudinary.com/dvmsllkkg/video/upload/v1658077496/apps/stay_ztjrhu.mp3",
     image: "imgs/stay.png",
   },
   {
     title: "Tom's Diner (Cover)",
     artist: "AnnenMayKantereit / Giant Rooks",
-    audio: "/music/toms_diner_cover.mp3",
+    audio:
+      "https://res.cloudinary.com/dvmsllkkg/video/upload/v1658077502/apps/annenMayKantereit_giant_rooks_vd8iox.mp3",
     image: "imgs/annenMayKantereit_giant.jpg",
   },
   {
     title: "No quiero Llorar (Cover)",
     artist: "Maikel Delacalle",
-    audio: "/music/no_quiero_llorar_cover.mp3",
+    audio:
+      "https://res.cloudinary.com/dvmsllkkg/video/upload/v1658077495/apps/no_quiero_llorar_u9esax.mp3",
     image: "imgs/maikel_delacalle.jpg",
   },
   {
     title: "Love Nwantiti (Lyrics)",
     artist: "Ckay",
-    audio: "/music/love_nwantiti_lyrics.mp3",
+    audio:
+      "https://res.cloudinary.com/dvmsllkkg/video/upload/v1658077488/apps/love_nwantiti_cfagbu.mp3",
     image: "imgs/ckay.jfif",
   },
   {
     title: "El Tiempo Pasa",
     artist: "Farruko",
-    audio: "/music/time_goes_by.mp3",
+    audio:
+      "https://res.cloudinary.com/dvmsllkkg/video/upload/v1658077502/apps/el_tiempo_pasa_eh8shc.mp3",
     image: "imgs/farruko.jpg",
   },
   {
@@ -46,19 +52,22 @@ export const sings = [
   {
     title: "More Than Words",
     artist: "Extreme",
-    audio: "/music/more_than_words.mp3",
+    audio:
+      "https://res.cloudinary.com/dvmsllkkg/video/upload/v1658077496/apps/more_than_words_w3ldoe.mp3",
     image: "imgs/more_than_words.png",
   },
   {
     title: "More Than You Know",
     artist: "❓",
-    audio: "/music/more_than_you_know.mp3",
+    audio:
+      "https://res.cloudinary.com/dvmsllkkg/video/upload/v1658077720/apps/more_than_you_know_uupscw.mp3",
     image: "imgs/scummbar.jpg",
   },
   {
     title: "Demons",
     artist: "Imagine Dragons",
-    audio: "/music/demons.mp3",
+    audio:
+      "https://res.cloudinary.com/dvmsllkkg/video/upload/v1658076971/apps/demons_qab3wi.mp3",
     image: "imgs/lechuck.jpg",
   },
 ];
